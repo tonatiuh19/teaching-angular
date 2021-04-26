8c0734cfcb4fa853412b106c5d047e5eb423808b
Tonatiuh Gomez <tonatiuh.gom@gmail.com>
2021-04-26 11:43:35 -0500

    using ngFor

--- base/src/app/superHeroes/list/list.component.ts
+++ base/src/app/superHeroes/list/list.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
-export class ListComponent implements OnInit {
+export class ListComponent {
+  heroes: string[] = ['Ironman', 'Cyclops', 'Hulk', 'Thor'];
+  heroeLeft: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  eraseSuper() {
+    const left: string = this.heroes.shift() || '';
+    this.heroeLeft = left;
   }
-
 }
