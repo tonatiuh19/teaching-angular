540d1b4cdc207dc03d8a590622102038976d3dc0
Tonatiuh Gomez <tonatiuh.gom@gmail.com>
2021-04-26 14:43:15 -0500

    playing with modules

--- base/src/app/app.module.ts
+++ base/src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
+import { CounterComponent } from './counter/counter/counter.component';
 import { SuperHeroesModule } from './superHeroes/superHeroes.module';
+import { CounterModule } from './counter/counter.module';
 
 @NgModule({
-  declarations: [AppComponent, CounterComponent],
-  imports: [BrowserModule, SuperHeroesModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, SuperHeroesModule, CounterModule],
   providers: [],
   bootstrap: [AppComponent],
 })
