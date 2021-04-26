e2316ece433b2e7cb3842700b7b550745fc14327
Tonatiuh Gomez <tonatiuh.gom@gmail.com>
2021-04-26 18:28:30 -0500

    Two way data binding with ngModel

--- base/src/app/avengers/main/main.component.ts
+++ base/src/app/avengers/main/main.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  name: string;
+  level: number;
+}
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css'],
 })
 export class MainComponent {
-  //Optional
-  /*add(e: any) {
-    e.preventDefault();
-  }*/
+  new: Personaje = {
+    name: 'Captain Marvel',
+    level: 95,
+  };
 
-  add() {}
+  add() {
+    console.log(this.new);
+  }
+
+  //Optional
+  /*changeName(e:any){
+    console.log(e.target.value);
+  }*/
 }
