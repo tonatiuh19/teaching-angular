e50262a7a128e7d16b21447d97539d150e70e07d
Tonatiuh Gomez <tonatiuh.gom@gmail.com>
2021-04-26 10:40:42 -0500

    creating a component in another folder

--- base/src/app/app.module.ts
+++ base/src/app/app.module.ts
@@ -2,17 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter.component';
+import { CounterComponent } from './counter/counter.component';
+import { SuperHeroeComponent } from './superHeroes/superHeroe/superHeroe.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CounterComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CounterComponent, SuperHeroeComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ base/src/app/superHeroes/superHeroe/superHeroe.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-superHeroe',
+  templateUrl: 'superHeroe.component.html',
+})
+export class SuperHeroeComponent {}
