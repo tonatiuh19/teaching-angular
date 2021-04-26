4d8ba23588422702ac83bb30959189364c65cb3a
Tonatiuh Gomez <tonatiuh.gom@gmail.com>
2021-04-26 11:13:57 -0500

    creating a component in an automatic way

--- base/src/app/app.module.ts
+++ base/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
 import { SuperHeroeComponent } from './superHeroes/superHeroe/superHeroe.component';
+import { ListComponent } from './superHeroes/list/list.component';
 
 @NgModule({
-  declarations: [AppComponent, CounterComponent, SuperHeroeComponent],
+  declarations: [AppComponent, CounterComponent, SuperHeroeComponent, ListComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
