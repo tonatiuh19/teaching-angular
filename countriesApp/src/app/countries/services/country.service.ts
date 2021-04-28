052436367cb5d78776c0744805c0434caa584dbb
Tonatiuh Gomez <tonatiuh.gom@gmail.com>
2021-04-28 12:43:03 -0500

    using interfaces in countries

--- countriesApp/src/app/countries/services/country.service.ts
+++ countriesApp/src/app/countries/services/country.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Country } from '../interfaces/country.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +9,9 @@ import { Observable } from 'rxjs';
 export class CountryService {
   constructor(private http: HttpClient) {}
 
-  searching(val: string): Observable<any> {
-    return this.http.get(`https://restcountries.eu/rest/v2/name/${val}`);
+  searching(val: string): Observable<Country[]> {
+    return this.http.get<Country[]>(
+      `https://restcountries.eu/rest/v2/name/${val}`
+    );
   }
 }
